276105720c4450d80e5e3d78d9b26c2c554407b7
Fernando Cesar da Silva <fcgpsjob@gmail.com>
2020-05-13 01:24:15 -0300

    Add Mutex for grant execution order

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const { filterCollection } = require('./util/filterCollection');
 const { generateGlobals } = require('./util/generateGlobal');
 const { getFilesInFolderPerExtension } = require("./util/getFilesInFolderPerExtension");
 const { format } = require("util");
+const { Mutex } = require('async-mutex');
 
 
 program
@@ -30,32 +31,39 @@ const environment = parseJsonFile(program.environment);
 
 const suites = getFilesInFolderPerExtension(program.suite, "suite");
 
+const mutex = new Mutex();
+
 suites.forEach(suite => {
-  const name = suite;
-  suite = parseJsonFile(suite);
+  mutex.acquire().then(function(release) {
+    const name = suite;
+    suite = parseJsonFile(suite);
 
-  const globals = generateGlobals(suite);
+    const globals = generateGlobals(suite);
 
-  filteredCollectionDefinition = filterCollection(collectionDefinition, suite);
+    filteredCollectionDefinition = filterCollection(collectionDefinition, suite);
 
-  const collection = new Collection(filteredCollectionDefinition).toJSON();
+    const collection = new Collection(filteredCollectionDefinition).toJSON();
 
-  newman({
-      collection: collection,
-      environment: environment,
-      globals: globals,
-  }, function (err, summary) {
-      if (err) { throw err; }
-      console.log(format('Suite %s run complete!', name));
-  }).on('beforeTest', function (err, summary) {
-      var arr = new Array();
-      arr[0] = "var config = pm.globals.get(pm.info.requestName);";
-      arr[1] = "if(config !== undefined)";
-      arr[2] = "{";
-      arr[3] = "    pm.test(\"Status test\", function () {";
-      arr[4] = "        pm.response.to.have.status(config);";
-      arr[5] = "    });";
-      arr[6] = "}";
-      summary.item.events.members.find(element => element.listen == "test").script.exec = summary.item.events.members.find(element => element.listen == "test").script.exec.concat(arr);
+    newman({
+        collection: collection,
+        environment: environment,
+        globals: globals,
+        reporters: [ 'cli' ]
+    }, function (err, summary) {
+        if (err) { throw err; }
+        console.log(format('Suite %s run complete!', name));
+    }).on('beforeTest', function (err, summary) {
+        var arr = new Array();
+        arr[0] = "var config = pm.globals.get(pm.info.requestName);";
+        arr[1] = "if(config !== undefined)";
+        arr[2] = "{";
+        arr[3] = "    pm.test(\"Status test\", function () {";
+        arr[4] = "        pm.response.to.have.status(config);";
+        arr[5] = "    });";
+        arr[6] = "}";
+        summary.item.events.members.find(element => element.listen == "test").script.exec = summary.item.events.members.find(element => element.listen == "test").script.exec.concat(arr);
+    }).on('done', function (err, summary) {
+        release();
+    });
   });
 });
