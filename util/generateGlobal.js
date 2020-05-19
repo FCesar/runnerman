19da035a4fbefe2c0f14f4c5c88552a105eb68f0
Fernando Cesar da Silva <fcgpsjob@gmail.com>
2020-05-19 19:36:55 -0300

    Refactor generateGlobal

--- util/generateGlobal.js
+++ util/generateGlobal.js
@@ -1,28 +1,18 @@
+const { Variable, VariableScope } = require("postman-collection");
+
 module.exports = {
-    
+    generateGlobals: (suite) => {
+        const variableScope = new VariableScope();
 
-    generateGlobals: suite => {
-        const values = [];
+        for (const [key, value] of Object.entries(suite)) {
+            const variable = new Variable({
+                "key": key,
+                "value": value
+            });
 
-        const globals = {
-            "id": "50f0331c-45be-4c45-8df9-75b70d27e1e9",
-            "values": [],
-            "name": "My Workspace Globals",
-            "_postman_variable_scope": "globals",
-            "_postman_exported_at": "2020-05-11T17:50:30.198Z",
-            "_postman_exported_using": "Postman/7.22.1"
-        };
-
-        for (const [k, v] of Object.entries(suite)) {
-            const value = {
-                "key": null,
-                "value": null,
-                "enabled": true
-            };
-
-            values.push(Object.assign(value, {"key": k, "value": v}));
+            variableScope.values.members.push(variable);
         }
 
-        return Object.assign(globals, { values: values })
+        return variableScope;
     }
 }
