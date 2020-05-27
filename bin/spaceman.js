ff5b8e79902fab16824981e5e8438dabb1766632
Fernando Cesar da Silva <fcgpsjob@gmail.com>
2020-05-27 00:19:59 -0300

    Refactor main function in bin/spaceman.js

--- bin/spaceman.js
+++ bin/spaceman.js
@@ -6,7 +6,6 @@ const { spaceman } = require("../");
 const { program } = require('commander');
 const { version } = require('../package.json');
 
-
 program
   .version(version)
   .option('-c, --collection <type>')
@@ -15,25 +14,28 @@ program
   .option('-i, --iterations <type>')
   .parse(process.argv);
 
-if (program.collection === '' ||
-  program.environment === '' ||
-  program.suite === '')
-{
-  program.help();
-}
+(async function main(program) {
+  if (program.collection === '' ||
+      program.environment === '' ||
+      program.suite === '')
+  {
+    program.help();
+  }
 
-(async function main() {
   const collection = parseJsonFile(program.collection);
 
   const environment = program.environment !== undefined && parseJsonFile(program.environment);
 
-  const suites = getFilesInFolderPerExtension(program.suite, "suite").map((suite) => {
-    return { "name": suite, "obj": parseJsonFile(suite) } ;
-  });
-
   const iterations = parseInt(program.iterations) || 1;
 
-  const exitCode = await spaceman(suites, collection, environment, iterations);
+  const suites = getFilesInFolderPerExtension(program.suite, "suite");
 
-  process.exit(exitCode);
-})();
+  for(const suite of suites) {
+    await spaceman({
+      "name": suite,
+      "obj": parseJsonFile(suite)
+    }, collection, environment, iterations);
+  };
+
+  process.exit(0);
+})(program);
