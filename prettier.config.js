1ee5fd274aa1100a9a145b8452fdf19d9e0fa083
Bruno Furtado <brunofurmon@gmail.com>
2020-05-30 18:39:14 -0300

    Add prettier configuration

---
+++ prettier.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    tabWidth: 4,
+    printWidth: 120,
+    useTabs: false,
+    semi: true,
+    singleQuote: true,
+    trailingComma: 'none',
+    bracketSpacing: true,
+    jsxBracketSameLine: false
+};
